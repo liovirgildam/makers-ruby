cb3049e11d9ce3ace68a6a2e4fb5baab7bbcefa6
Liovirgilda Mendonca <liovirgilda_developer@gmail.com>
2022-10-24 18:10:48 +0100

    Add comments to the code, install simpleCov, add tests to when withdraw amount is larger than balance

--- tech_test/lib/bank_tech.rb
+++ tech_test/lib/bank_tech.rb
@@ -1,33 +1,51 @@
 class Bank
-    def initialize(io)
-        @io = io
+    def initialize()
+        # initial balance in user's account
         @balance = 0
-        @movements = []
+
+        # array that saves all user's transactions
+        @transactions = []
     end
     
-
+    # Method that checks user's balance
     def balance
         @balance
     end
 
+    # Method that allows user to deposit money to its account 
+    # Takes two parameters - transaction's date(DD-MM-YYYY) and amount in integer or float
     def deposit(date, amount)
+        # updates balance to reflect deposit
         @balance = @balance + amount
-        @movements.unshift( 
+
+        # Adds transaction to beginning of the array 
+        @transactions.unshift( 
+
+            # Adds an Hash to the array with transaction details 
+            # sprintf - turns integer into a float with two decimal points then converts it into a string
             { date: date, credit: sprintf('%.2f', amount), debit: "" , balance: sprintf('%.2f', @balance) }
         )
     end
 
+    # Method that allows user to withdraw money to its account
     def withdraw(date, amount)
+       
+        # throws error if not enough funds in the account
+        fail "Non-Sufficient funds" if (@balance - amount) < 0
         @balance = @balance - amount
-        @movements.unshift( 
+
+        # updates balance to reflect withdraw
+        @transactions.unshift( 
             { date: date, credit: "" , debit: sprintf('%.2f', amount), balance: sprintf('%.2f', @balance) }
         )
     end
 
+    # Method that prints all user's account transactions from most recent to the oldest
     def statement
-        @io.puts "date || credit || debit || balance"
-        @movements.each do |movement|
-            @io.puts "#{movement[:date]} || #{movement[:credit]} || #{movement[:debit]} || #{movement[:balance]}"
+
+        puts "date || credit || debit || balance"
+        @transactions.each do |transaction|
+            puts "#{transaction[:date]} || #{transaction[:credit]} || #{transaction[:debit]} || #{transaction[:balance]}"
         end
     end
 
--- tech_test/spec/bank_tech_spec.rb
+++ tech_test/spec/bank_tech_spec.rb
@@ -1,57 +1,56 @@
 require "bank_tech"
 
 RSpec.describe Bank do
-    it "makes an initial deposit of 1000 pounds to a bank account" do
-        io = double :io
-        account = Bank.new(io)
+    it "user makes an initial deposit of 1000 pounds to its bank account" do
+        account = Bank.new
         account.deposit("20/10/2022", 1000)
         
         expect(account.balance).to eq 1000.00
-        expect(io).to receive(:puts).with( "date || credit || debit || balance").ordered
-        expect(io).to receive(:puts).with( "20/10/2022 || 1000.00 ||  || 1000.00").ordered
-        account.statement
     end
 
-    it "makes two consecutives deposits 1000 and 2000 to a bank account" do
-        io = double :io
-        account = Bank.new(io)
+    it "user makes two consecutives deposits of 1000 and 2000 to its bank account" do
+        account = Bank.new
         account.deposit("20/10/2022", 1000)
         account.deposit("21/10/2022", 2000)
         
         expect(account.balance).to eq 3000.00
-        expect(io).to receive(:puts).with( "date || credit || debit || balance").ordered
-        expect(io).to receive(:puts).with( "21/10/2022 || 2000.00 ||  || 3000.00").ordered
-        expect(io).to receive(:puts).with( "20/10/2022 || 1000.00 ||  || 1000.00").ordered
-        account.statement
-
     end
 
-    it "makes a withdraw of 500 after a deposit of 1000" do
-        io = double :io
-        account = Bank.new(io)
+    it "user makes two consecutives deposits of 250.50 and 249.49" do
+        account = Bank.new
+        account.deposit("20/10/2022", 250.50)
+        account.deposit("21/10/2022", 249.49)
+        
+        expect(account.balance).to eq 499.99
+    end
+
+    it "user makes a withdraw of 500 after a deposit of 1000" do
+        account = Bank.new
         account.deposit("20/10/2022", 1000)
         account.withdraw("22/10/2022", 500)
         
         expect(account.balance).to eq 500.00
-        expect(io).to receive(:puts).with( "date || credit || debit || balance").ordered
-        expect(io).to receive(:puts).with( "22/10/2022 ||  || 500.00 || 500.00").ordered
-        expect(io).to receive(:puts).with( "20/10/2022 || 1000.00 ||  || 1000.00").ordered
-        account.statement
     end 
 
+    context "throws an error if user tries to withdraw an amount larger than account balance" do
+        it "fails" do
+            account = Bank.new
+            account.deposit("20/10/2022", 1000)
+            expect{ account.withdraw("21/10/2022", 1100)}.to raise_error "Non-Sufficient funds"
+        end
+    end
+
     it "prints account statement to the terminal" do
-        io = double :io
-        account = Bank.new(io)
+        account = Bank.new
         account.deposit("20/10/2022", 1000)
         account.deposit("21/10/2022", 2000)
         account.withdraw("24/11/2022", 500)
         
         expect(account.balance).to eq 2500
-        expect(io).to receive(:puts).with( "date || credit || debit || balance").ordered
-        expect(io).to receive(:puts).with( "24/11/2022 ||  || 500.00 || 2500.00").ordered
-        expect(io).to receive(:puts).with( "21/10/2022 || 2000.00 ||  || 3000.00").ordered
-        expect(io).to receive(:puts).with( "20/10/2022 || 1000.00 ||  || 1000.00").ordered
-        account.statement
-
+        expect{account.statement}.to output(
+            "date || credit || debit || balance\n" +
+            "24/11/2022 ||  || 500.00 || 2500.00\n" + 
+            "21/10/2022 || 2000.00 ||  || 3000.00\n" +
+            "20/10/2022 || 1000.00 ||  || 1000.00\n").to_stdout
     end
 end
