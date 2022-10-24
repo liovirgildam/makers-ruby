9d1046f357ac7fcd646f0bdee9450da7a248eb76
Liovirgilda Mendonca <liovirgilda_developer@gmail.com>
2022-10-24 13:08:06 +0100

    passed first test

--- tech_test/lib/bank_tech.rb
+++ tech_test/lib/bank_tech.rb
@@ -1,2 +1,19 @@
 class Bank
+    def initialize
+        @balance = 0
+        @movements = []
+    end
+    
+
+    def balance
+        @balance
+    end
+
+    def deposit(date, amount)
+        @balance = @balance + amount
+        @movements.unshift( 
+            { date: date, credit: "", deposit: amount, balance: @balance }
+        )
+    end
+
 end
