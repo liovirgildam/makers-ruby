d37ffccbae0e11953b2db3a28bbcfa8ffc03e17d
Liovirgilda Mendonca <liovirgilda_developer@gmail.com>
2022-10-24 13:10:11 +0100

    second test- deposits twice

--- tech_test/spec/bank_tech_spec.rb
+++ tech_test/spec/bank_tech_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Bank do
         account.deposit("20/10/2022", 1000)
         expect(account.balance).to eq 1000
     end
+
+    it "makes two consecutives deposits 1000 and 2000 to a bank account" do
+        account = Bank.new
+        account.deposit("20/10/2022", 1000)
+        account.deposit("21/20/2022", 2000)
+        expect(account.balance).to eq 3000
+    end
 end
